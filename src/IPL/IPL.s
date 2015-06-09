6129a9551562c009c6ce26f7fa587a13fd06ef17
xuzhenglun <xuzl@xuzl-debian.WORKGROUP>
2015-06-09 13:06:02 +0800

    first day,IPL

---
+++ src/IPL/IPL.s
@@ -0,0 +1,57 @@
+;Goo-OS
+;TAB=4
+
+    ORG     0x7c00
+
+
+;以下为FAT12软盘文件系统代码
+    JMP     entry
+    DB      0x90
+
+    DB      "INITIPL "
+    DW      512
+    DB      1
+    DW      1
+    DB      2
+    DW      224
+    DW      2880
+    DB      0xf0
+    DW      9
+    DW      18
+    DW      2
+    DD      0
+    DD      2880
+    DB      0,0,0x29
+    DD      0xffffffff
+    DB      "Goo-OS     "
+    DB      "FAT12   "
+    RESB    18
+
+;程序主体
+entry:
+    MOV     AX,0
+    MOV     SS,AX
+    MOV     SP,0x7c00
+    MOV     DS,AX
+    MOV     ES,AX
+    MOV     SI,msg
+
+putloop:
+    MOV     AL,[SI]
+    ADD     SI,1
+    CMP     AL,0
+    JE      fin
+    MOV     AH,0x0e
+    MOV     BX,15
+    INT     0x10
+    JMP     putloop
+
+fin:
+    HLT
+    JMP     fin
+
+msg:
+    DB      0x0a,0x0a
+    DB      "hello,world"
+    DB      0x0a
+    DB      0
